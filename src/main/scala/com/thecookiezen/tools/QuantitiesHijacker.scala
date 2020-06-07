9137ffe8da982de62e61db80a4f6b85e55aedf9e
kornelrabczak <kornel.rabczak@gmail.com>
2020-06-07 20:37:11 +0200

    Removes debug from QuantitiesHijacker

--- src/main/scala/com/thecookiezen/tools/QuantitiesHijacker.scala
+++ src/main/scala/com/thecookiezen/tools/QuantitiesHijacker.scala
@@ -1,32 +1,13 @@
 package com.thecookiezen.tools
 
 import scala.collection.mutable
-import scala.tools.nsc.Global
 import scala.reflect.internal.util.Statistics
-import java.lang.reflect.Field
-import java.lang.Class
+import scala.tools.nsc.Global
 
 object QuantitiesHijacker {
   type Quantities = mutable.HashMap[String, Statistics#Quantity]
   def getRegisteredQuantities[G <: Global](global: G): Quantities = {
-    val claszz = classOf[Statistics]
-    
-    
-    val fields = claszz.getDeclaredFields()
-
-    fields.foreach( field =>
-      println(field.getName())
-    )
-
-    println("methods: ")
-
-    val methods = claszz.getDeclaredMethods()
-
-    methods.foreach( field =>
-      println(field.getName())
-    )
-
-    val field = claszz.getDeclaredField("scala$reflect$internal$util$Statistics$$qs")
+    val field = classOf[Statistics].getDeclaredField("scala$reflect$internal$util$Statistics$$qs")
     field.setAccessible(true)
     field.get(global.statistics).asInstanceOf[Quantities]
   }
